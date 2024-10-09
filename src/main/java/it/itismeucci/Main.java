e3e3d0b08bbf1a1fd1e44689452b55bada864232
Niccolo <niccolomancini3@gmail.com>
2024-10-09 09:11:22 +0200

    fine client

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,6 +1,9 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -9,5 +12,12 @@ public class Main {
         System.out.println("Hello world!");
         Socket s = new Socket("localhost", 5673);
         System.out.println("il client si è collegato");
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        out.writeBytes("ciaoo");
+        String stringaRicevuta = in.readLine();
+        System.out.println("la stringa ricevuta: " + stringaRicevuta);
+        
+        s.close();
     }
 }
