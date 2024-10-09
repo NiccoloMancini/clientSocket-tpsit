33b2b0c29c4aadc407b8bd28fc992af262addedc
Niccolo <niccolomancini3@gmail.com>
2024-10-09 09:51:56 +0200

    modifica client do while

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -6,17 +6,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
         Socket s = new Socket("localhost", 5673);
         System.out.println("il client si è collegato");
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        out.writeBytes("ciaoo\n");
-        String stringaRicevuta = in.readLine();
-        System.out.println("la stringa ricevuta: " + stringaRicevuta);
+        String stringaRicevuta;
+        String stringaScritta;
+        do {
+            System.out.print("inserire la stringa (! per terminare la connesione): ");
+            stringaScritta = scanner.nextLine();
+            out.writeBytes(stringaScritta + "\n");
+            stringaRicevuta = in.readLine();
+            System.out.println("la stringa ricevuta: " + stringaRicevuta);
+        } while (!stringaScritta.equals("!"));
         
         s.close();
     }
