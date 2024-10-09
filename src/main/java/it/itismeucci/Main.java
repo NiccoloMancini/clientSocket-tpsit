2ed7b1bcb9836e71250b42347bca176527b502c5
Niccolo <niccolomancini3@gmail.com>
2024-10-09 08:53:14 +0200

    clientSocket created

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,13 @@
 package it.itismeucci;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Hello world!");
+        Socket s = new Socket("localhost", 5673);
+        System.out.println("il client si è collegato");
     }
 }
